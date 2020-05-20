e00b0636d4d67f85ba19febe5cc2d19946e589d3
Heikki Hämäläinen <heikki.hamalainen@solita.fi>
2020-05-20 13:59:36 +0300

    Add minimal Macchiato server

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :dependencies [[com.taoensso/timbre "4.10.0"]
                  [org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.520"]])
+                 [org.clojure/clojurescript "1.10.520"]
+                 [macchiato/core "0.2.16"]
+                 [macchiato/env "0.0.6"]
+                 [mount "0.1.16"]
+                 [metosin/reitit "0.5.1"]
+                 [metosin/spec-tools "0.10.3"]])
--- src/macchiato_test/core.cljs
+++ src/macchiato_test/core.cljs
@@ -1,5 +1,18 @@
 (ns macchiato-test.core
-  (:require [taoensso.timbre :refer [info]]))
+  (:require [taoensso.timbre :refer [info]]
+            [macchiato.server :as http]))
+
+(defn handler
+  [request callback]
+  (callback {:status 200
+             :body "Hello Macchiato"}))
 
 (defn server []
-  (info "Hey I am running now!"))
+  (info "Hey I am running now!")
+  (let [host "127.0.0.1"
+        port 3000]
+    (http/start
+      {:handler    handler
+       :host       host
+       :port       port
+       :on-success #(info "macchiato-test started on" host ":" port)})))
